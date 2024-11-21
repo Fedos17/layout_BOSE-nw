d0a9229e5dd53a45649328f0e9763fc341ed5e23
Fedos17 <dfkd81@gmail.com>
2024-11-21 15:55:29 +0200

    Add image. Create contsiner, grid, flickity.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,23 @@
 'use strict';
+
+// document.addEventListener('DOMContentLoaded', function() {
+//   const carousel = document.querySelector('.carousel');
+//   const prev = document.querySelector('.car-prev');
+//   const next = document.querySelector('.car-next');
+
+//   // eslint-disable-next-line no-undef
+//   const flkty = new Flickity(carousel, {
+//     cellAlign: 'left',
+//     contain: true,
+//     autoPlay: true,
+//     prevNextButtons: false,
+//   });
+
+//   prev.addEventListener('click', function() {
+//     flkty.previous();
+//   });
+
+//   next.addEventListener('click', function() {
+//     flkty.next();
+//   });
+// });
