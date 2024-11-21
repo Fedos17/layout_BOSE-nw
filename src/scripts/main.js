fdd5e3ce905c399c2a833ac2f2b3f13581b01c36
Fedos17 <dfkd81@gmail.com>
2024-11-21 19:19:08 +0200

    Modify mixins, reset, container. Add top, header

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -21,3 +21,14 @@
 //     flkty.next();
 //   });
 // });
+
+const phoneLink = document.querySelector('.top__phone-img');
+const phoneTooltip = document.querySelector('.top__phone-tooltip');
+
+phoneLink.addEventListener('mouseenter', function() {
+  phoneTooltip.classList.remove('top__phone-tooltip--hover');
+});
+
+phoneLink.addEventListener('mouseleave', function() {
+  phoneTooltip.classList.add('top__phone-tooltip--hover');
+});
