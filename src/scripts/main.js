b9547a21fe08494bb81fe35ff8fdfd0648c0db1f
Fedos17 <dfkd81@gmail.com>
2024-11-23 14:17:02 +0200

    Add block About Us

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,27 +1,5 @@
 'use strict';
 
-// document.addEventListener('DOMContentLoaded', function() {
-//   const carousel = document.querySelector('.carousel');
-//   const prev = document.querySelector('.car-prev');
-//   const next = document.querySelector('.car-next');
-
-//   // eslint-disable-next-line no-undef
-//   const flkty = new Flickity(carousel, {
-//     cellAlign: 'left',
-//     contain: true,
-//     autoPlay: true,
-//     prevNextButtons: false,
-//   });
-
-//   prev.addEventListener('click', function() {
-//     flkty.previous();
-//   });
-
-//   next.addEventListener('click', function() {
-//     flkty.next();
-//   });
-// });
-
 const phoneLink = document.querySelector('.top__phone-img');
 const phoneTooltip = document.querySelector('.top__phone-tooltip');
 
